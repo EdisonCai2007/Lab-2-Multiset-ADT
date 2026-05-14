5d6be59aad632fcf332b4d9abad1681e116cf7b9
Edison Cai <thrillergriller1234@gmail.com>
2026-05-14 19:41:48 -0400

    modify count

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
@@ -15,7 +16,18 @@ public class Tree {
         return this._root == null;
     }
 
-    private static int count() {
-
+    public int count(Integer item) {
+        if (this.is_empty()) {
+            return 0;
+        } else {
+            int num = 0;
+            if (Objects.equals(this._root, item)) {
+                num += 1;
+            }
+            for (Tree subtree: this._subtrees) {
+                num += subtree.count(item);
+            }
+            return num;
+        }
     }
 }
