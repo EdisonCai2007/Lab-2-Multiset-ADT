eb7cf5e51c110570547ca6cf7506ad2716b03bf9
dorothy <83884354+dotoroto@users.noreply.github.com>
2026-05-14 19:29:20 -0400

    Implemented is_empty()

--- src/Tree.java
+++ src/Tree.java
@@ -3,11 +3,15 @@ import java.util.ArrayList;
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
 
-    private int _root;
+    private Integer _root;
     private ArrayList<Tree> _subtrees;
 
-    Tree(int root, ArrayList<Tree> subtrees) {
+    Tree(Integer root, ArrayList<Tree> subtrees) {
         _root = root;
         _subtrees = subtrees;
+    }
+
+    public boolean is_empty(){
+        return this._root == null;
     }
 }
