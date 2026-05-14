c7f833dbf2a1f5f21aab50c6a00d026f2404bfca
Edison Cai <thrillergriller1234@gmail.com>
2026-05-14 19:19:51 -0400

    create-init

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    private int _root;
+    private ArrayList<Tree> _subtrees;
+
+    Tree(int root, ArrayList<Tree> subtrees) {
+        _root = root;
+        _subtrees = subtrees;
+    }
 }
